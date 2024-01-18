67371c7133d44e2e8ea41911a989a1618738e57e
Yamin <yamin@MacBook-Pro-de-Yamin.local>
2024-01-18 18:33:32 +0100

    Sephora style

--- frontend/dashboard/src/routes/+page.svelte
+++ frontend/dashboard/src/routes/+page.svelte
@@ -14,24 +14,24 @@
 
 </script>
 
-<div class="navbar bg-base-300 rounded-box">
-    <div class="flex-1 px-2 lg:flex-none">
-      <a class="text-lg font-bold">Dashboard</a>
-    </div>
-    <div class="flex justify-end flex-1 px-2">
-      <div class="flex items-stretch">
-        <!-- <a class="btn btn-ghost rounded-btn">Button</a> -->
-        <div class="dropdown dropdown-end">
-          <div tabindex="0" role="button" class="btn btn-ghost rounded-btn">Select by timeline</div>
-          <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-            <li><a>Week</a></li>
-            <li><a>Month</a></li>
-            <li><a>Year</a></li>
-          </ul>
-        </div>
+<div class="navbar rounded-box" style="background-color: white;">
+  <div class="flex justify-center w-full" style="font-family: 'Gill Sans'">
+    <div class="text-lg" style="font-size: 24px;">S E P H O R A</div>
+  </div>
+  </div><br>
+  <div class="flex justify-end flex-1 px-2">
+    <div class="flex items-stretch">
+      <!-- <a class="btn btn-ghost rounded-btn">Button</a> -->
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost rounded-btn">Select by timeline</div>
+        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+          <li><a>Week</a></li>
+          <li><a>Month</a></li>
+          <li><a>Year</a></li>
+        </ul>
       </div>
     </div>
-  </div><br>
+  </div>
 
 <br>
 <br>
