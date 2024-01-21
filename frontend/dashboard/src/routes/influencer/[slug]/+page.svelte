01942ecba75646124990982a54605dc5d4312e97
thoo-ma <trobin@student.42.fr>
2024-01-21 21:35:11 +0100

    mask

--- frontend/dashboard/src/routes/influencer/[slug]/+page.svelte
+++ frontend/dashboard/src/routes/influencer/[slug]/+page.svelte
@@ -33,16 +33,9 @@
         <div class="navbar-center">
           <div class="text-lg" style="font-size: 24px;">S E P H O R A</div>
         </div>
-        <!-- <div class="navbar-end">
-            <div class="timeline-select-container">
-            <select class="select select-bordered select-sm max-w-xs" bind:value={selectedTimeline}>
-              <option selected>Month</option>
-              <option>Week</option>
-              <option>Day</option>
-            </select>
-          </div>
-        </div> -->
-      </div>
+        <div class="navbar-end">
+        </div>
+    </div>
 
     <br>
     <br>
@@ -50,48 +43,13 @@
     <div class="product-container">
 
       <div class="card lg:card-side bg-base-100 shadow-xl">
-        <figure><img src={influencer?.image} alt="Album"/></figure>
+        <figure>
+            <img class="mask mask-circle" src="http://localhost:8000/images/{influencer?.name}" alt="ProfilePicture"/>
+        </figure>
         <div class="card-body">
           <h2 class="card-title">{influencer?.name}</h2>
+          <h2 class="card-title">ig followers: {influencer?.ig_followers}</h2>
           <br>
-          <div class="stats shadow" style="overflow: hidden;">
-
-            <div class="stat">
-              <div class="stat-figure text-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-              </div>
-              <div class="stat-title">Pruduct Likes</div>
-              <!-- <div class="stat-value text-primary">{productData?.likes}K</div> -->
-              <div class="stat-value text-primary">0K</div>
-              <div class="stat-desc">likes on social media</div>
-            </div>
-
-            <div class="stat">
-              <div class="stat-figure text-secondary">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-              </div>
-              <div class="stat-title">Product Comment</div>
-              <!-- <div class="stat-value text-secondary">{productData?.comment}K</div> -->
-              <div class="stat-value text-secondary">0K</div>
-              <div class="stat-desc">comment on social media</div>
-            </div>
-
-            <div class="stat">
-              <div class="stat-figure text-secondary">
-                <div class="avatar online">
-                  <div class="w-16 rounded-full">
-                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                  </div>
-                </div>
-              </div>
-              <div class="stat-title">Most share by</div>
-              <!-- {#if productData?.influencer}
-              {#each productData.influencer.slice(0, 3) as influencer, index}
-                  <span style="color:cornflowerblue">@{influencer}</span>
-              {/each}
-              {/if} -->
-            </div>
-          </div>
           <br>
         <h1 class="text-lg" style="font-family:'Gill Sans'">Details :</h1>
 
